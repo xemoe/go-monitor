2d0b933dd279dbbdc1d2c3f543d8b311cdde3590
rob121 <rob121@gmail.com>
2020-06-30 14:55:26 -0400

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -3,8 +3,8 @@
 
 echo "Installing for linux"
 
-wget -O /usr/local/bin/  "http://github"
-chmod +x /usr/local/bin/go/monitor
+wget -O /usr/local/bin/go-monitor  "https://github.com/rob121/go-monitor/releases/download/v.0.1/go-monitor-linux-amd64"
+chmod +x /usr/local/bin/go-monitor
 mkdir /etc/gomonitor
 cp ../sample.config.yml /etc/gomonitor/
 cp gomonitor.service /etc/systemd/system/
